4d9abb541afe6a8d66b8c5416c3f6c09a2f2ed1e
risonakamo <hmykanae@gmail.com>
2018-09-04 17:11:21 -0400

    things are happening

--- wdmain.jsx
+++ wdmain.jsx
@@ -1,18 +1,56 @@
 //WhatDo main
 export class WdMain extends React.Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.addChoice=this.addChoice.bind(this);
+    this.mainKeys=this.mainKeys.bind(this);
+
+    this.state={
+      choices:[]
+    };
+
+    this.choicesId=0;
+  }
+
+  addChoice()
+  {
+    this.choicesId++;
+    this.state.choices.push(this.choicesId);
+    this.setState({choices:this.state.choices});
+  }
+
+  //main key handler
+  mainKeys(e)
+  {
+    if (e.key=="Enter")
+    {
+      e.preventDefault();
+      this.addChoice();
+    }
+  }
+
   render()
   {
     return (<>
-      <div className="title" contentEditable="">どうしよう</div>
-      <Choice number={1}/>
-      <Choice number={2}/>
+      <div className="title"
+        contentEditable=""
+        onKeyDown={this.mainKeys}
+      >
+        どうしよう
+      </div>
+
+      {this.state.choices.map((x,i)=>{
+        return <Choice number={i} key={x} mainKeys={this.mainKeys}/>;
+      })}
     </>);
   }
 }
 
-//choice(int number)
+//choice(int number,function mainKeys)
 //number: the number of the choice that appears
+//mainKeys: function from parent
 class Choice extends React.Component
 {
   render()
@@ -20,7 +58,7 @@ class Choice extends React.Component
     return (
       <div className="choice">
         <span>{this.props.number}</span>
-        <div className="input" contentEditable="">sample</div>
+        <div className="input" contentEditable="" onKeyDown={this.props.mainKeys}>sample</div>
       </div>
     );
   }
