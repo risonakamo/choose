81936d7b7ef377483fcf9a2a7922aef5489b8bb6
risonakamo <hmykanae@gmail.com>
2018-09-04 14:10:51 -0400

    trying out gulp watch

--- compile.js
+++ compile.js
@@ -2,6 +2,14 @@ const gulp=require("gulp");
 const less=require("gulp-less");
 const babel=require("gulp-babel");
 
-gulp.src("*.less",{base:"."}).pipe(less()).pipe(gulp.dest("."));
+console.log("gulp is watching");
 
-gulp.src("*.jsx",{base:"."}).pipe(babel({presets:["@babel/preset-react"]})).pipe(gulp.dest("."));
+gulp.watch("*.less",()=>{
+    gulp.src("*.less",{base:"."}).pipe(less()).pipe(gulp.dest("."));
+    console.log("compiled less");
+});
+
+gulp.watch("*.jsx",()=>{
+    gulp.src("*.jsx",{base:"."}).pipe(babel({presets:["@babel/preset-react","minify"]})).pipe(gulp.dest("."));
+    console.log("compiled jsx");
+});
--- wdmain.js
+++ wdmain.js
@@ -1,10 +1,1 @@
-//WhatDo main
-export class WdMain extends React.Component {
-  render() {
-    return React.createElement("div", {
-      className: "title",
-      contentEditable: ""
-    }, "\u3069\u3046\u3057\u3088\u3046");
-  }
-
-}
+export class WdMain extends React.Component{render(){return React.createElement("div",{className:"title",contentEditable:""},"\u3069\u3046\u3057\u3088\u3046")}}
